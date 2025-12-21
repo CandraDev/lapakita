a25dad544424f62ec4fa8e7bc7b0aec32397ad8f
CandraDev <candrasetiadev@gmail.com>
2025-12-21 18:47:11 +0700

    Project Initialization

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+        hmr: {
+            host: '127.0.0.1',
+            protocol: 'ws',
+            port: 5173,
+        },
+    },
+});
