d88112e4436ba59560ffc7077970139322d82a11
Abdan <ghoyatulabdan992@gmail.com>
2026-02-09 14:22:27 +0700

    api(account): add login-register-logout API

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
 
 Route::get('/categories', [\App\Http\Controllers\UtilityController::class, 'getCategories'])->name('api.categories');
 Route::get('/regencies/{province}', [\App\Http\Controllers\UtilityController::class, 'regencies']);
